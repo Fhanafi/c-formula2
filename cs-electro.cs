07f15c28f48f97331ba3406d05af2b8a8a24a41b
Han <89624707+Fhanafi@users.noreply.github.com>
2022-10-14 15:58:03 +0700

    update cs-electro.cs

--- cs-electro.cs
+++ cs-electro.cs
@@ -1,8 +1,11 @@
 using System;
-class HelloWorld {
+class mencariVolt {
   static void Main() {
     Console.Write("berapa ampere : ");
-    int amp = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("nilai amp : {0}", amp);
+    double amp = Convert.ToDouble(Console.ReadLine());
+    Console.Write("berapa Ohm : ");
+    int ohm = Convert.ToInt32(Console.ReadLine());
+    double v = amp * ohm;
+    Console.WriteLine("hasil = {0} volt", v);
   }
 }
