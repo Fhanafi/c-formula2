0338a4b12001a3867e3eb41b891fee4c37ab75a9
Han <89624707+Fhanafi@users.noreply.github.com>
2022-10-14 14:56:39 +0700

    update c-math.c

--- c-math.c
+++ c-math.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
-int main()
-{
-    printf("Hello World");
+void luas() {
+   int luasnya,panjang,lebar;
+   printf("masukan panjang benda : ");
+   scanf("%d", &panjang);
+   printf("masukan lebar benda : ");
+   scanf("%d", &lebar);
+   if (!panjang) 
+   {
+      printf("angka yang dimasukan salah \n");
+   } else if(!lebar) {
+      printf("angka yang dimasukan salah \n");
+   }
+   luasnya = (panjang * lebar);
+   printf("luas benda adalah %d m2", luasnya);
+}
 
-    return 0;
+void main() {
+  luas();
 }
