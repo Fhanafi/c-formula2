430b9bc09e5c3f4ac12f43954a5ae328f086c085
Han <89624707+Fhanafi@users.noreply.github.com>
2022-10-14 23:07:25 +0700

    update cs-electro.cs

--- cs-electro.cs
+++ cs-electro.cs
@@ -5,11 +5,6 @@ class mencariVolt {
     double amp = Convert.ToDouble(Console.ReadLine());
     Console.Write("berapa Ohm : ");
     int ohm = Convert.ToInt32(Console.ReadLine());
-    if(!amp) {
-      Console.WriteLine("Salah memasukan angka");
-    } else if(!ohm) {
-      Console.WriteLine("Salah memasukan angka");
-    }
     double v = amp * ohm;
     Console.WriteLine("hasil = {0} volt", v);
   }
