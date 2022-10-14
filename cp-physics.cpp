9c3a6c37ad8918548f171284ac6699cdaedd46dc
Han <89624707+Fhanafi@users.noreply.github.com>
2022-10-14 15:02:38 +0700

    update cp-physics.cpp

--- cp-physics.cpp
+++ cp-physics.cpp
@@ -4,7 +4,14 @@ using namespace std;
 
 int main()
 {
-    cout<<"Hello World";
-
-    return 0;
+    int f,m,a;
+   printf("masukan masa benda : ");
+   scanf("%d",&m);
+   printf("masukan kecepatan : ");
+   scanf("%d",&a);
+   if(!m && !a) {
+      printf("salah memasukan angka");
+   }
+   f = (m * a);
+   printf("hasilnya adalah %d newton",f);
 }
